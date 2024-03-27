56b61ec6b207fb3feb4928563493f774bc586c7c
afcalvo4 <142455626+afcalvo4@users.noreply.github.com>
2024-03-27 16:46:39 -0500

    ac228
    Lab 7

--- ParkingGarage/src/ParkingGarage.jl
+++ ParkingGarage/src/ParkingGarage.jl
@@ -3,6 +3,9 @@ module ParkingGarage
 include("core.jl")
 include("sim.jl")
 
-export ParkingGarageSOW, simulate, StaticPolicy, AdaptivePolicy
+export ParkingGarageSOW, simulate, simulatelevels,
+                         simulatebin, simulatelevelsbin,
+                         simulate_double,simulatelevels_double,
+                         StaticPolicy, AdaptivePolicy
 
 end # module ParkingGarage
--- ParkingGarage/src/sim.jl
+++ ParkingGarage/src/sim.jl
@@ -1,10 +1,3 @@
-"""
-Demand on opening day is for 750 spaces, and rises linearly at the rate of `demand_growth_rate` spaces/ year
-"""
-function calculate_demand(t, demand_growth_rate::AbstractFloat)
-    return 750 + demand_growth_rate * (t - 1)
-end
-
 """The capacity of the parking garage is 200 spaces per level"""
 calculate_capacity(x::ParkingGarageState) = 200 * x.n_levels
 
@@ -28,11 +21,11 @@ end
 """
 If we are following the static (fixed) policy, then the rule is simple. We add `n_levels` in the first year, and then keep the same number of levels in all subsequent years.
 """
-function get_action(x::ParkingGarageState, policy::StaticPolicy)
+function get_action(x::ParkingGarageState, policy::StaticPolicy, Δ)
     if x.year == 1
         return ParkingGarageAction(policy.n_levels)
     else
-        return ParkingGarageAction(0)
+        return ParkingGarageAction(Δ)
     end
 end
 
@@ -40,20 +33,54 @@ end
 If we are following the adaptive policy, then the rule is slightly more complicated.
 We add `n_levels` in the first year. Then, every future year we compare the capacity and demand. If the demand is greater than the capacity, we add a level.
 """
-function get_action(x::ParkingGarageState, policy::AdaptivePolicy)
-    # THIS IS THE FUNCTION YOU NEED TO REPLACE!
-    throw("You need to implement the adaptive policy yourself")
+function get_action(x::ParkingGarageState, policy::AdaptivePolicy, Δ)
+    if x.year == 1
+        return ParkingGarageAction(policy.n_levels_init)
+    else
+        if calculate_capacity(x) < x.demand
+            return ParkingGarageAction(Δ)
+        else
+            return ParkingGarageAction(0)
+        end
+    end
 end
+
+function get_actionbin(x::ParkingGarageState, policy::AdaptivePolicy)
+    if x.year == 1
+        return ParkingGarageAction(policy.n_levels_init)
+    else
+        if calculate_capacity(x) < x.demand
+            return ParkingGarageAction(trunc(Int, round(rand(1)[1])))
+        else
+            return ParkingGarageAction(0)
+        end
+    end
+end
+function get_action_double(x::ParkingGarageState, policy::AdaptivePolicy)
+    if x.year == 1
+        return ParkingGarageAction(policy.n_levels_init)
+    else
+        if calculate_capacity(x) < 0.50 * x.demand
+            return ParkingGarageAction(x.n_levels)
+        else
+            return ParkingGarageAction(0)
+        end
+    end
+end
+
 
 """
 Run the simulation for a single year
 """
 function run_timestep(
-    x::ParkingGarageState, s::ParkingGarageSOW, policy::T
+    x::ParkingGarageState, s::ParkingGarageSOW, Δ, policy::T
 ) where {T<:AbstractPolicy}
 
+    # calculate the demand for this year
+    x.demand = calculate_demand(x.year, s.demand_growth_rate)
+
     # the very first step is to decide on the action
-    a = get_action(x, policy)
+    a = get_action(x, policy,Δ)
 
     # next we have to do stuff to implement the action in our model!    
     # construction costs are higher for initial construction if we are using the adaptive policy
@@ -65,8 +92,7 @@ function run_timestep(
 
     # revenue -- you can only sell parking spaces that you have AND that are wanted
     capacity = calculate_capacity(x)
-    demand = calculate_demand(x.year, s.demand_growth_rate)
-    revenue = 11_000 * min(capacity, demand)
+    revenue = 11_000 * min(capacity, x.demand)
 
     # lease costs are fixed
     lease_cost = 3_600_000
@@ -81,10 +107,79 @@ function run_timestep(
     return profit
 end
 
+function run_timestepbin(
+    x::ParkingGarageState, s::ParkingGarageSOW, policy::T
+) where {T<:AbstractPolicy}
+
+    # calculate the demand for this year
+    x.demand = calculate_demand(x.year, s.demand_growth_rate)
+
+    # the very first step is to decide on the action
+    a = get_actionbin(x, policy)
+
+    # next we have to do stuff to implement the action in our model!    
+    # construction costs are higher for initial construction if we are using the adaptive policy
+    is_adaptive = policy isa AdaptivePolicy && x.year == 1
+    constr_costs = calc_construction_cost(x.n_levels, a.Δn_levels, is_adaptive)
+
+    # and update the system to reflect our action!
+    x.n_levels += a.Δn_levels
+
+    # revenue -- you can only sell parking spaces that you have AND that are wanted
+    capacity = calculate_capacity(x)
+    revenue = 11_000 * min(capacity, x.demand)
+
+    # lease costs are fixed
+    lease_cost = 3_600_000
+
+    # operating costs depend only on the capacity
+    operating_costs = 2_000 * capacity
+
+    # total costs
+    costs = constr_costs + lease_cost + operating_costs
+
+    profit = revenue - costs
+    return profit
+end
+
+function run_timestep_double(
+    x::ParkingGarageState, s::ParkingGarageSOW, policy::T
+) where {T<:AbstractPolicy}
+
+    # calculate the demand for this year
+    x.demand = calculate_demand(x.year, s.demand_growth_rate)
+
+    # the very first step is to decide on the action
+    a = get_action_double(x, policy)
+
+    # next we have to do stuff to implement the action in our model!    
+    # construction costs are higher for initial construction if we are using the adaptive policy
+    is_adaptive = policy isa AdaptivePolicy && x.year == 1
+    constr_costs = calc_construction_cost(x.n_levels, a.Δn_levels, is_adaptive)
+
+    # and update the system to reflect our action!
+    x.n_levels += a.Δn_levels
+
+    # revenue -- you can only sell parking spaces that you have AND that are wanted
+    capacity = calculate_capacity(x)
+    revenue = 11_000 * min(capacity, x.demand)
+
+    # lease costs are fixed
+    lease_cost = 3_600_000
+
+    # operating costs depend only on the capacity
+    operating_costs = 2_000 * capacity
+
+    # total costs
+    costs = constr_costs + lease_cost + operating_costs
+
+    profit = revenue - costs
+    return profit
+end
 """
 Run the simulation for `s.n_years` years
 """
-function simulate(s::ParkingGarageSOW, policy::T) where {T<:AbstractPolicy}
+function simulate(s::ParkingGarageSOW,Δ, policy::T) where {T<:AbstractPolicy}
 
     # initialize the model
     x = ParkingGarageState()
@@ -93,12 +188,100 @@ function simulate(s::ParkingGarageSOW, policy::T) where {T<:AbstractPolicy}
     years = collect(1:(s.n_years))
     profits = map(years) do year
         x.year = year
-        run_timestep(x, s, policy)
+        run_timestep(x, s, Δ, policy)
     end
 
     # apply discounting
     discount_weights = @. (1 - s.discount_rate)^(years - 1)
     npv_profits = sum(profits .* discount_weights)
     npv_profits_millions = npv_profits / 1e6
+
     return npv_profits_millions
+end
+
+function simulatelevels(s::ParkingGarageSOW, Δ, policy::T) where {T<:AbstractPolicy}
+
+    # initialize the model
+    x = ParkingGarageState()
+
+    # calculate the profits for each year
+    years = collect(1:(s.n_years))
+    level_policy = map(years) do year
+        x.year = year
+        run_timestep(x, s, Δ, policy)
+        x.n_levels
+    end
+    
+    return level_policy
+end
+
+function simulatebin(s::ParkingGarageSOW, policy::T) where {T<:AbstractPolicy}
+
+    # initialize the model
+    x = ParkingGarageState()
+
+    # calculate the profits for each year
+    years = collect(1:(s.n_years))
+    profits = map(years) do year
+        x.year = year
+        run_timestepbin(x, s, policy)
+    end
+
+    # apply discounting
+    discount_weights = @. (1 - s.discount_rate)^(years - 1)
+    npv_profits = sum(profits .* discount_weights)
+    npv_profits_millions = npv_profits / 1e6
+
+    return npv_profits_millions
+end
+function simulatelevelsbin(s::ParkingGarageSOW, policy::T) where {T<:AbstractPolicy}
+
+    # initialize the model
+    x = ParkingGarageState()
+
+    # calculate the profits for each year
+    years = collect(1:(s.n_years))
+    level_policy = map(years) do year
+        x.year = year
+        run_timestepbin(x, s, policy)
+        x.n_levels
+    end
+    
+    return level_policy
+end
+
+function simulate_double(s::ParkingGarageSOW, policy::T) where {T<:AbstractPolicy}
+
+    # initialize the model
+    x = ParkingGarageState()
+
+    # calculate the profits for each year
+    years = collect(1:(s.n_years))
+    profits = map(years) do year
+        x.year = year
+        run_timestep_double(x, s, policy)
+    end
+
+    # apply discounting
+    discount_weights = @. (1 - s.discount_rate)^(years - 1)
+    npv_profits = sum(profits .* discount_weights)
+    npv_profits_millions = npv_profits / 1e6
+
+    return npv_profits_millions
+end
+
+function simulatelevels_double(s::ParkingGarageSOW, policy::T) where {T<:AbstractPolicy}
+
+    # initialize the model
+    x = ParkingGarageState()
+
+    # calculate the profits for each year
+    years = collect(1:(s.n_years))
+    level_policy = map(years) do year
+        x.year = year
+        run_timestep_double(x, s, policy)
+        x.n_levels
+    end
+    
+    return level_policy
 end
